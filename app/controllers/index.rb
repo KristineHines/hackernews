935890c3bf9dbf5f56f9bdd26581d422525ec134
Apprentice <apprentice@dbc02.local>
2013-05-01 15:04:44 -0700

    created login/user views. Added Bcrypt to user model. Added routes to index.rb.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,28 @@
 get '/' do
-  # Look in app/views/index.erb
+  @posts = Post.all
   erb :index
 end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  @posts = Post.all
+  @user = User.authenticate(params[:username], params[:password])
+  if @user
+    erb :index
+  else
+    @errors = "incorrect username or password"
+    erb :login
+  end
+end
+
+post '/create_account' do
+  @posts = Post.all
+  @user = User.create(params)
+
+  erb :index
+end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,41 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts
+
+  include BCrypt
+
+
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(name, password)
+    @user = find_by_username(name)
+     if @user
+      @user.password == password
+      @user
+    else
+      nil
+    end
+
+  end
+
+  # def self.authenticate(username, password)
+  #   @user = User.find_by_email(username)
+  #   if @user
+  #     @user.password == password
+  #   else
+  #     nil
+  #   end
+
+  # end
 end
+
+
